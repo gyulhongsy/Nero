fa58c75cff84651dfd84b7efe68362469a4728f6
Fornamin <97093507+Fornamin@users.noreply.github.com>
2023-08-24 23:19:51 +0900

    update: 대사창 변경

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -26,6 +26,13 @@ public class PlayerMove : MonoBehaviour
     private int nextStageIndex = 0;
     private int currentStageIndex = 0;
 
+    // 오브젝트 스캔
+    float direction;
+    float detectRange = 2.0f;
+    GameObject scanObject;
+
+    public DialogueManager manager;
+
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -34,6 +41,8 @@ public class PlayerMove : MonoBehaviour
     }
     void Start()
     {
+        rigid = GetComponent<Rigidbody2D>();
+
         // 현재 스테이지의 인덱스를 찾아냅니다.
         string currentSceneName = SceneManager.GetActiveScene().name;
         currentStageIndex = nextStageNames.IndexOf(currentSceneName);
@@ -55,6 +64,17 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetButton("Horizontal"))
         {
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == 1;
+
+            if (Input.GetAxisRaw("Horizontal") == -1.0)
+            {
+                // Debug.Log("left");
+                direction = -1.0f;
+            }
+            else
+            {
+                // Debug.Log("right");
+                direction = 1.0f;
+            }
         }
 
         //Moblie
@@ -118,6 +138,35 @@ public class PlayerMove : MonoBehaviour
                     anim.SetBool("isJumping", false);
             }
         }
+
+        //Debug.DrawRay(rigid.position, new Vector3(direction * detectRange, (float)0.0), new Color(0, 0, 1));
+
+        RaycastHit2D rayHitDetect = Physics2D.Raycast(rigid.position, new Vector3(direction, (float)0.0), detectRange, LayerMask.GetMask("Object"));
+
+        if (rayHitDetect.collider != null)
+        {
+            // 물체를 인식했을 때
+            scanObject = rayHitDetect.collider.gameObject;
+
+            // 해당하는 명령어가 보이게 작성해주시면 됩니다
+
+            // Debug.Log("scan : " + scanObject.name);
+        }
+        else
+        {
+            scanObject = null;
+        }
+
+        // T키를 누르면 스캔된 물체 대사 진행 -> 후에 삭제
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (scanObject != null)
+            {
+                Debug.Log("T key : " + scanObject.name);
+                manager.Action(scanObject.name);
+            }
+        }
+
     }
     //Mobile
     public void ButtonDown(string type)
@@ -154,6 +203,8 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
+    /*
+     * 오류 때문에 잠시 주석 처리 해뒀습니다 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("warmHole"))
@@ -162,6 +213,7 @@ public class PlayerMove : MonoBehaviour
             LoadNextStage();
         }
     }
+    */
 
     void LoadNextStage()
     {
